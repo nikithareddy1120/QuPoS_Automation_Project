f51def63c2e91f7225ae6a30aa8d42ac03463f65
nreddy <nikitha.reddy@jackinthebox.com>
2024-11-28 10:44:32 +0530

    Updated batch file

--- RunTests.bat
+++ RunTests.bat
@@ -2,4 +2,4 @@
 
 whoami /groups | find "S-1-5-32-544" > nul && echo Running as Admin || echo Not running as Admin
 
-behave --tags=qupos -f html -o Reports/htmlReport/report.html
+behave --tags=qpos -f html -o Reports/htmlReport/report.html
